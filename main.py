c5387339f930278186a9c58fc803e1723285b0de
Vivaan100 <Vivaan.Jagtiani28@bcp.org>
2024-10-04 09:47:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ class Game:
         self.screen.fill(WHITE)
         self.all_sprites.draw(self.screen)
         self.draw_text(self.screen, str(self.dt*1000), 24, WHITE, WIDTH/30, HEIGHT/30)
-        self.draw_text(self.screen, "This game is awesome...", 24, BLACK, WIDTH/2, HEIGHT/24)
+        self.draw_text(self.screen, "This game is sooooo...awesome...", 24, BLACK, WIDTH/2, HEIGHT/24)
         pg.display.flip()
 
 # checks file name and creates a game object
