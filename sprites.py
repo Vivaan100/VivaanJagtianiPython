488af09984740c6d94396341b97fe1cc54692d94
Vivaan100 <Vivaan.Jagtiani28@bcp.org>
2024-10-11 13:06:32 -0700

    Updated sprites and added coins and Poerups

--- sprites.py
+++ sprites.py
@@ -19,6 +19,7 @@ class Player(Sprite):
         self.y = y*TILESIZE
         self.speed = 10
         self.vx, self.vy = 0,0
+        self.coins = 0
     def get_keys(self):
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
@@ -29,14 +30,52 @@ class Player(Sprite):
             self.rect.vy += self.speed
         if keys[pg.K_d]:
             self.rect.vx += self.speed
+    def collide_with_walls(self, dir):
+        if dir =='x':
+            hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.left - TILESIZE
+                    #self.x = hits[0].rect.left - self.rect.width
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+                self.vx = 0
+                self.rect.x = self.x
+        if dir =='y':
+            hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
+            if hits:
+                if self.vy > 0:
+                    self.y = hits[0].rect.top - TILESIZE
+                    #self.x = hits[0].rect.top - self.rect.height
+                if self.vy < 0:
+                    self.y = hits[0].rect.bottom
+                self.vy = 0
+                self.rect.y = self.y
+    def collide_with_ftuff(self, grou, kill):
+        hits = pg.sprite.spritecollide(self, group, kill)
+        if hits: 
+            if str(hits[0].__.class__.__name__) = "Powerup":
+                print("i hit a powerup...")
+                self.speed =+ 5
+            if str(hits[0].__class__.__name__) == "Coin":
+                print("i hit a coin...")
+                self.coins +=1
+            
 
     def update(self):
         self.get_keys()
         self.x += self.vx*self.game.dt
-        self.y += self.vy*self.game
+        self.y += self.vy*self.game.dt
 
         # self.rect.x += 1
+        self.collide_with_stuff(self.game.all_powerups, True)
+        self.collide_with_stuff(self.game.all_coins, True)
 
+        self.rect.x = self.x
+        self.collide_with_walls('x')
+
+        self.rect.y = self.y
+        self.collide_with_walls('y')
 
 class Mob(Sprite):
     def __init__(self, game, x, y):
@@ -77,8 +116,8 @@ class Wall(Sprite):
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.rect = self.image.get_rect()
         self.image.fill(BLUE)
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
 
     def update(self):
         pass
