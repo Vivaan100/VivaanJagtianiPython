9b4bc4bc5d4765847a51f19ce7b2272dd1cf508a
Vivaan100 <Vivaan.Jagtiani28@bcp.org>
2024-10-30 20:20:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ import pygame as pg
 from settings import *
 from sprites import *
 from tilemap import *
-from os import path
+from os import *
+import sys
 from random import randint
+'''
+GOALS: Get to the middle square without touching any enemies
+RULES: Get a find a powerup to not get eating by an enemy
+FEEDBACK: collide with enemy before eat powerup you die
+FREEDOM: Move around
 
 
-# created a game class to use later
-# it will have all important parts for the game to run
-# the class is needed to organize the different parts of the game
+
+
+
+'''
+
+
+
+# Game class to organize the various parts of the game
 class Game:
-    # The game init method initializes all the necessary components for the game which includes video and sound
-    # this includes the game clock(sets framerate: what is a framerate???)
     def __init__(self):
         pg.init()
         pg.mixer.init()
@@ -22,88 +31,61 @@ class Game:
         pg.display.set_caption("Vivaan's Game")
         self.clock = pg.time.Clock()
         self.running = True
-    # create player block, creates the all_sprites group so they can batch update and render, defines properties that can be seen in the game system
-    #
+
     def load_data(self):
         self.game_folder = path.dirname(__file__)
         self.map = Map(path.join(self.game_folder, 'level1.txt'))
+
     def new(self):
-        self.load_data()
+        self.load_data()# Loads all of the sprites 
         self.all_sprites = pg.sprite.Group()
         self.all_walls = pg.sprite.Group()
         self.all_mobs = pg.sprite.Group()
         self.all_powerups = pg.sprite.Group()
         self.all_coins = pg.sprite.Group()
-        # COMMENTED THIS PIECE OF CODE OUT(WE CAN USE IT LATER BY UNCOMMENTING IT)
-        # self.player = Player(self, 1, 1)
-        # instantiated a mob
-        # self.mob = Mob(self, 100,100)
-        # makes new mobs and walls using a for loop
-        # for i in range(randint(10,20)):
-        #     m = Mob(self, i*randint(0, 200), i*randint(0, 200))
-        #     Wall(self, i*TILESIZE, i*TILESIZE)
-        
-        # takes map.data and parses(???) using enumerate, so that we can assign x,y values to object instances.
-        for row, tiles in enumerate(self.map.data):
-            print(row)
+
+        # Parse the map data and create game objects
+        for row, tiles in enumerate(self.map.data):# connects to level1.txt
             for col, tile in enumerate(tiles):
-                print(col)
                 if tile == '1':
                     Wall(self, col, row)
-                if tile == 'P':
+                elif tile == 'P':
                     self.player = Player(self, col, row)
-                if tile == 'M':
+                elif tile == 'M':
                     Mob(self, col, row)
-    # using self.running as a boolean to continue running the game
+
     def run(self):
         while self.running:
-            self.dt = self.clock.tick(FPS) / 1000
+            self.dt = self.clock.tick(FPS) / 1000# the amount of time
             self.events()
             self.update()
             self.draw()
-        # input
+
     def events(self):
         for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    self.running = False
+            if event.type == pg.QUIT:
+                self.running = False# Ends the game
 
-        # pg.quit() if something happens in the event, the game will quit.
-        # process
     def update(self):
         self.all_sprites.update()
-        # output
-        pass
-    # CREATING THE FONT, TEXT SIZE, COLOR OF THE TEXT, ETC. 
-    def draw_text(self, surface, text, size, color, x, y):
+
+    def draw_text(self, surface, text, size, color, x, y):# the font, colors, size, of the words that show up on the screen
         font_name = pg.font.match_font('arial')
         font = pg.font.Font(font_name, size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
-        text_rect.midtop = (x,y)
+        text_rect.midtop = (x, y)
         surface.blit(text_surface, text_rect)
-        # CREATES TEXT WHEN YOU PRINT THE GAME, LIKE THE TIMER AND A HEADING AT THE TOP
+
     def draw(self):
         self.screen.fill(WHITE)
         self.all_sprites.draw(self.screen)
-        self.draw_text(self.screen, str(self.dt*1000), 24, WHITE, WIDTH/30, HEIGHT/30)
-        self.draw_text(self.screen, "This game is sooooo...awesome...", 24, BLACK, WIDTH/2, HEIGHT/24)
+        self.draw_text(self.screen, str(int(self.dt * 1000)), 24, WHITE, WIDTH / 30, HEIGHT / 30)
+        self.draw_text(self.screen, "This game is sooooo...awesome...", 24, BLACK, WIDTH / 2, HEIGHT / 24)
         pg.display.flip()
 
-# checks file name and creates a game object
+# Checks file name and creates a game object
 if __name__ == "__main__":
     g = Game()
-    # create all game elements with the new method (not function)
-    g.new()
-    # run the game...
-    g.run()
-
-        
-
-    g.new()
-
-    g.run()           
-
-
-
-
-                
+    g.new()  # Initialize game elements
+    g.run()  # Start the game
